5e49c3787dce008da193dc8158c15e9e6f3fc8c4
aslak <aslak.bakkeland@everyday.no>
2022-11-10 22:24:26 +0100

    whichkey no broke

--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -22,10 +22,8 @@ local config = {
       name = "file", -- optional group name
       f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
       w = { "<cmd>Telescope live_grep<cr>", "Find Word" },
-      r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap=false, buffer = 123 }, -- additional options for creating the keymap
+      r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File"}, -- additional options for creating the keymap
       c = { "<cmd>Telescope resume<cr>", "Resume telescope search" },
-      n = { "New File" }, -- just a label. don't create any mapping
-      b = { function() print("bar") end, "Foobar" } -- you can also pass functions!
     },
   }
 }
